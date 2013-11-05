322159e14d438d700aa6b7a6d4e5da4d7def6256
yiannisk <ikaradimas@hotmail.com>
2013-11-05 08:19:18 +0200

    Added juery.mustache, first take on the orders template.

--- Projects/ETravel.Coffee.UI.Site/Scripts/orders.js
+++ Projects/ETravel.Coffee.UI.Site/Scripts/orders.js
@@ -1,13 +1,34 @@
 $(function () {
 	renderOrders();
 
+	function renderOrder(orderData) {
+		$.Mustache.load('./Templates/Order.htm')
+		    .done(function () {
+		        $('.orders-list').mustache('order', orderData, 'append');
+
+				var $currentOrder = $('.orders-list .order:last');
+
+				$currentOrder.on('click', function () {
+					console.log('order click');
+					$('.orders-list .order').removeClass('selected');
+					$(this).addClass('selected');
+				});
+		    });
+	}
+
 	function renderOrders() {
+		$('.orders-list').html('');
+
 		$.ajax({
 			type: "GET",
 			dataType: "json",
 			url: coffee.app.settings.serviceLocator + "/orders",
 			success: function (data) {
-				console.log(data);
+				var i, currentOrder;
+
+				for(i = 0; i < data.length; i++) {					
+					renderOrder(data[i]);
+				}
 			}
 		});
 	}
