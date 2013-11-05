3154690cd59db7a05188bbc76f5351d4d6b66d67
yiannisk <ikaradimas@hotmail.com>
2013-11-05 17:51:31 +0200

    First take on create order form

--- Projects/ETravel.Coffee.UI.Site/Scripts/resource.js
+++ Projects/ETravel.Coffee.UI.Site/Scripts/resource.js
@@ -8,11 +8,11 @@ function Resource(name, url, actions, options) {
 	this.url = url || coffee.app.settings.serviceLocator + '/' + name;
 	
 	for(i = 0, currentAction = actions[i]; i < actions.length; i ++) {
-		this[currentAction] = function (callback) {
+		this[currentAction] = function (callback, methodOptions) {
 			var callback = callback, 
 				action = currentAction;
 
-			$.ajax($.extend(options, {
+			$.ajax($.extend(options, methodOptions, {
 				url: self.url, 
 				type: action, 
 				success: function (data) {
--- Projects/ETravel.Coffee.UI.Site/Site.Master.cs
+++ Projects/ETravel.Coffee.UI.Site/Site.Master.cs
@@ -8,17 +8,10 @@ namespace ETravel.Coffee.UI.Site
 	public partial class SiteMaster : MasterPage
 	{
 		protected internal Settings Settings { get; set; }
-		protected internal IDictionary<string, string> Links { get; set; }
 
 		public SiteMaster()
 		{
 			Settings = Settings.Default;
-
-			Links = new Dictionary<string, string>
-			{
-				{ "Home", "Default.aspx" },
-				{ "About", "About.aspx" }
-			};
 		}
 
 		protected void Page_Load(object sender, EventArgs e)
